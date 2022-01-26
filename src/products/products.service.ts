3036cb3b90df0fb4aeb1e93bea38d9d5c242937c
ikhsan0000 <ikhsanfirdauz000@gmail.com>
2022-01-26 15:34:31 +0700

    Multiple File Upload Endpoint

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entitiy';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+
+    constructor(@InjectRepository(Product) private readonly productRepository: Repository<Product>
+    ) { }
+
+
+    async getAll() {
+        return this.productRepository.find({ relations: ['attendees'] });
+    }
+}
